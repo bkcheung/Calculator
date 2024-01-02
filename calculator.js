4b8664db9226b3b72bc11ab3ad6457bc20f33c0b
bkcheung <b.cheung@hotmail.com>
2024-01-02 16:31:38 -0500

    Add subtract, multiply, and divide functions.

--- calculator.js
+++ calculator.js
@@ -3,10 +3,39 @@ function add(a,b){
         return a+b;
     }
     else{
-        throw new Error("Must input two numbers to add");
+        throw new Error("Both inputs must be numbers");
+    }
+}
+
+function subtract(a,b){
+    if(typeof a==='number' && typeof b==='number'){
+        return a-b;
+    }
+    else{
+        throw new Error("Both inputs must be numbers");
+    }
+}
+
+function multiply(a,b){
+    if(typeof a==='number' && typeof b==='number'){
+        //returns result rounded to 3 decimal places
+        return Math.round(a*b*1000)/1000;
+    }
+    else{
+        throw new Error("Both inputs must be numbers");
+    }
+}
+
+function divide(a,b){
+    if(typeof a==='number' && typeof b==='number'){
+        //returns result rounded to 3 decimal places
+        return Math.round(a/b*1000)/1000;
+    }
+    else{
+        throw new Error("Both inputs must be numbers");
     }
 }
 
 //testing area
-const result = add(10,2);
+const result = divide(10,2.2);
 console.log(result);
