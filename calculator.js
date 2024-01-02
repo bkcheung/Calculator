a925e95c2f3c04b8befc7f646dba8c758db2bd15
bkcheung <b.cheung@hotmail.com>
2024-01-02 17:05:46 -0500

    Add operate function with switch cases for adding, subtracting, dividing, multiplying

--- calculator.js
+++ calculator.js
@@ -36,6 +36,29 @@ function divide(a,b){
     }
 }
 
+function operate(num1,num2,operator){
+    if(typeof(operator)==='string'){
+        switch(operator){
+            case '+':
+                return add(num1,num2);
+                break;
+            case '-':
+                return subtract(num1,num2);
+                break;
+            case '/':
+                return divide(num1,num2);
+                break;
+            case '*':
+                return multiply(num1,num2);
+                break;
+        }
+    }
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = '+';
+
 //testing area
-const result = divide(10,2.2);
+const result = operate(10,2.22,'/');
 console.log(result);
